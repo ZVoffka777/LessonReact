16aad1b0cf5abb576635ad574b52109d62bab55e
Алексей <60385530+ZVoffka777@users.noreply.github.com>
2021-05-30 21:43:44 +0300

    Add files via upload

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.png';
+
+const Header = () => {
+	return <header className="header">
+			<img src={logo} alt={'logo'} width={40} height={30}/>
+		</header>
+}
+ export default Header;
