3cc212a7eda3f59e2d5384fb43c95114520bfada
Алексей <60385530+ZVoffka777@users.noreply.github.com>
2021-05-30 21:45:22 +0300

    Delete мое приложение/src/составные части directory

--- мое приложение/src/составные части/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from './logo.png';
-
-const Header = () => {
-	return <header className="header">
-			<img src={logo} alt={'logo'} width={40} height={30}/>
-		</header>
-}
- export default Header;
