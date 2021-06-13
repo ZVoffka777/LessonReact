27c3e02bceef2e8b7acf4fe8ece7451bdabe2b8d
ZVoffka777 <panteley.alex@gmail.com>
2021-06-13 23:38:58 +0300

    Lesson 19 react-router-dom

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
-//let c1 = "item";
-//let c2 = "active";
-// let classes = c1 + " " + c2;
-//let cassesNew = `${c1} ${c2}`;
-
 const Navbar = () => {
 	return <nav className={s.nav}>
-			<div className={s.item}><a>Profile</a></div>
-			<div className={`${s.item} ${s.active}`}><a>Messages</a></div>
+			<div className={s.item}><a href="/profile">Profile</a></div>
+			<div className={`${s.item} ${s.active}`}><a href="/dialogs">Messages</a></div>
 			<div className={s.item}><a>News</a></div>
 			<div className={s.item}><a>Music</a></div>
 			<div className={s.item}><a>Settings</a></div>
