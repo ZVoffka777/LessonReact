42ce6331f16978d3416755202fce81cc84e27706
ZVoffka777 <panteley.alex@gmail.com>
2021-06-19 15:53:48 +0300

    Lesson 21 path Routecle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
@@ -19,8 +20,9 @@ const App = () => {
 				<Header/>
 				<Navbar/>
 				<div className='app-wrapper-content'>
-					<Route path='/dialogs/'component={Dialogs}/>
+					<Route path='/news'component={News}/>
 					<Route path='/profile'component={Profile}/>
+					<Route path='/dialogs'component={Dialogs}/>
 				</div>
 			</div>
 		</BrowserRouter>);
--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 	return <nav className={s.nav}>
 			<div className={s.item}><a href="/profile">Profile</a></div>
 			<div className={`${s.item} ${s.active}`}><a href="/dialogs">Messages</a></div>
-			<div className={s.item}><a>News</a></div>
+			<div className={`${s.item} ${s.active}`}><a href="/news">News</a></div>
 			<div className={s.item}><a>Music</a></div>
 			<div className={s.item}><a>Settings</a></div>
 		</nav>
